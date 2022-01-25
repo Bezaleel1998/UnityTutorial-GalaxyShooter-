d0fddbf8594c6b17b5a427bfe066a6ca0fa2797a
NightHawk1998 <bezaleelairbornedivision@yahoo.com>
2022-01-25 22:32:41 +0700

    Section 12: User Interface (UI)
    - Score and Highscore Display
    - Score and Highscore Mechanic
    - Player Display Mechanic and UI
    - MainGame and GameOver Panel
    - GameOver text flicker (changing colour)
    - font style space (Additional)

--- Galaxy Shooter/Assets/Scripts/GameManager.cs
+++ Galaxy Shooter/Assets/Scripts/GameManager.cs
@@ -1,14 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     public static bool _isGamePaused = false;
 
+    [Header("Private Variable")]
+    private int _playerScore = 0;
+    [SerializeField] private int _points = 10;
+    [SerializeField] private Sprite[] _liveSprites;
+
+    [Header("UI Element")]
+    public GameObject mainGameContainer;
+    public GameObject gameOverContainer;
+    public Text gameOverText;
+    public Text highscoreText;
+    public Text scoreText;
+    public Image livesDisplay;
+
+    private void Awake()
+    {
+
+        gameOverContainer.SetActive(false);
+        scoreText.text = "Score : " + 0;
+        livesDisplay.sprite = _liveSprites[3];
+
+    }
 
     void Update()
+    {
+
+        KeyboardController();
+
+    }
+
+    void KeyboardController()
     {
 
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -21,6 +50,8 @@ public class GameManager : MonoBehaviour
 
     }
 
+    #region PauseMechanic
+
     public void PauseController()
     {
 
@@ -31,7 +62,7 @@ public class GameManager : MonoBehaviour
 
     void PauseGame()
     {
-        
+
         if (_isGamePaused)
         {
             Time.timeScale = 0f;
@@ -42,5 +73,74 @@ public class GameManager : MonoBehaviour
         }
 
     }
+
+    #endregion
+
+    #region ScoringSystem
+
+    public void AddScore()
+    {
+
+        _playerScore += _points;
+        Debug.Log("Player Score : " + _playerScore);
+
+        int _highScore = PlayerPrefs.GetInt("HighScore");
+        highscoreText.text = "HighScore = " + _highScore.ToString();
+
+        if (_playerScore > _highScore)
+        {
+
+            PlayerPrefs.SetInt("HighScore", _playerScore);
+            highscoreText.text = "HighScore = " + _highScore.ToString();
+
+        }
+
+        ShowScore(_playerScore);
+
+    }
+
+    void ShowScore(int playerScore)
+    {
+
+        scoreText.text = "Score : " + playerScore.ToString();
+
+    }
+
+    #endregion
+
+    #region PlayerLiveIndicator
+
+    public void PlayerDeadIndicator()
+    {
+        //when player dead 
+        //show the lives display to 0
+        livesDisplay.sprite = _liveSprites[0];
+        //show gameover panel
+        gameOverContainer.SetActive(true);
+        StartCoroutine(GameOverTextAnimation());
+        //pause the game
+
+    }
+
+    IEnumerator GameOverTextAnimation()
+    {
+
+        while (true)
+        {
+
+            gameOverText.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            yield return new WaitForSeconds(.5f);
+        }
+
+    }
+
+    public void PlayerLiveIndicator(int playerHp)
+    {
+
+        livesDisplay.sprite = _liveSprites[playerHp];
+
+    }
+
+    #endregion
 
 }
